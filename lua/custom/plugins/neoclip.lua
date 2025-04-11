8338f52fa912893c33b56c04aa5d02d29a42247a
Edvard Birkeland <ebl@datarespons.no>
2025-04-11 13:56:34 +0200

    WIP neoclip get OS clipboard entry

--- lua/custom/plugins/neoclip.lua
+++ lua/custom/plugins/neoclip.lua
@@ -24,5 +24,24 @@ return {
     }
 
     vim.api.nvim_set_keymap('n', '<leader>p', ':Telescope neoclip<CR>', { noremap = true, silent = true })
+
+    -- Autocmd to capture OS clipboard content and insert it into neoclip’s history.
+    -- Limitation: This will only fetch the newest OS clipboard entry.
+    -- TODO: This duplicates latest copy if it occured within Neovim
+    vim.api.nvim_create_autocmd({ 'FocusGained' }, {
+      callback = function()
+        local clipboard_content = vim.fn.getreg '+'
+        if clipboard_content and clipboard_content ~= '' then
+          local storage = require 'neoclip.storage'
+          local filetype = vim.bo.filetype or ''
+          local data = {
+            regtype = 'v',
+            contents = vim.split(clipboard_content, '\n', { plain = true }),
+            filetype = filetype,
+          }
+          storage.insert(data, 'yanks')
+        end
+      end,
+    })
   end,
 }
